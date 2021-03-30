fe3e4b7128d3f4f649f93931f2406851c4939a09
Sergio <sergio@MacBook-Pro-de-Sergio.local>
2021-03-30 16:19:02 -0500

    slick carousel

--- app/index.js
+++ app/index.js
@@ -7,3 +7,13 @@ var menuNav = document.querySelector('.main-nav');
 menuButton.addEventListener('click', function (event) {
   menuNav.classList.toggle("visible-menu");
 });
+
+$(document).ready(function(){
+  $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
